ba41986b30722823478d232a5101e4340c062471
Anderson A. de Souza <anderson-de-souza@outlook.com>
2025-09-06 11:12:03 -0300

    first steps

---
+++ add_item.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+
+        <div>
+            <label for="itemName">
+                <input type="text" name="itemName" id="itemName" placeHolder="Name">
+            </label>
+        </div>
+
+        <div>
+            <label for="itemDescription">
+                <input type="text" name="itemDescription" id="itemDescription" placeHolder="Description">
+            </label>
+        </div>
+
+        <div>
+            <label for="itemPrice">
+                <input type="text" name="itemPrice" id="itemPrice" placeHolder="Price">
+            </label>
+        </div>
+
+        <div>
+            <label for="itemImageUrl">
+                <input type="text" name="itemImageUrl" id="itemImageUrl" placeHolder="Image Address">
+            </label>
+        </div>
+
+        <div>
+            <input type="submit" value="Send">
+        </div>
+
+    </form>
+</body>
+</html>
